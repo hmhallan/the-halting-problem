212b484dcffc218558c7b9a74162de54bc356d44
Hallan Medeiros <hallan.medeiros@tjsc.jus.br>
2018-10-26 14:11:44 -0300

    Ajuste para executar teste unitário

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,17 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
  
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
  
     public static void main(String[] args) throws IOException {
- 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
- 
+    	java.io.Reader input = new InputStreamReader(System.in);
+		Main.start(input);
+    }
+    
+    public static void start( Reader input ) throws FileNotFoundException, IOException {
+    	
     }
  
 }
---
+++ src/test/java/TestHalt.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TestHalt {
+	
+	@Test
+	public void testa_problema_parada() throws IOException {
+		java.io.Reader input = new FileReader("src/main/resources/parada.in");
+		Main.start(input);
+	}
+
+}
